da2577b578c678c1e0f0dd40d9e3e677eae37488
jugrajn <jugraj.nijjar@gmail.com>
2021-06-13 23:05:45 -0400

    created managers prompt questions

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const Manager = require('./lib/manager.js');
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/Intern');
+
+// Manager prompt questions
+const managerQuestions = [
+    {
+        type: 'input',
+        message: 'What is the name of the manager?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "Please type in your id",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "Please provide your email",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "Please provide your office number?",
+        name: 'officeNumber'
+    }
+];
+
+// Employee prompt questions
+const employeeQuestions = [
+    {
+        
+    }
+]
---
+++ src/generateProfilePage.js
@@ -0,0 +1,3 @@
+// Create a Function that renders Manager Card
+
+// Create a Function that renders Employee Card
