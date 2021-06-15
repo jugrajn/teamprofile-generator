83a5ebb263e7fdb88c68ba656470386a27de33cb
jugrajn <jugraj.nijjar@gmail.com>
2021-06-14 23:11:01 -0400

    created initialize function to retrieve all input data manager and employees into an empty array

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const Manager = require('./lib/manager.js');
 const Engineer = require('./lib/engineer.js');
 const Intern = require('./lib/Intern');
-const { inherits } = require('util');
+
+const teamData = [];
 
 // Manager prompt questions
 const managerQuestions = [
@@ -57,13 +58,13 @@ const employeeQuestions = [
         type: 'input',
         message: "What is the engineer's github username?",
         name: 'gitHub',
-        when: role === 'engineer',
+        when: (input) => input.role === 'Engineer',
     },
     {
         type: 'input',
         message: "What is the name of the intern's school?",
         name: 'school',
-        when: role === 'intern',
+        when: (input) => input.role === 'Intern',
     },
     {
         type: 'confirm',
@@ -73,15 +74,30 @@ const employeeQuestions = [
 ]
 
 // Class to intialize promps
-// function init() {
-//     inquirer.prompt(managerQuestions)
-//             .then((response) => {
-            
-//             });
+function init() {
+    inquirer.prompt(managerQuestions)
+    // Retrieve Input values for Manager Questions into empty array
+            .then((managerData) => {
+            const {name, id, email, officeNumber } = managerData;
+            const manager = new Manager (name, id, email, officeNumber);
+
+            teamData.push(manager);
+            console.log(manager);
+            });
     
-//     inquirer.prompt(employeeQuestions)
-//             .then((response2) => {
+     // Retrive Input values for Employee questions into empty array       
+    inquirer.prompt(employeeQuestions)
+            .then((employeeData) => {
+            const {role, name, id, email, gitHub, school} = employeeData;
+            let theEmployee;
+            
+            // SET A CONDITION ON WHICH INPUT DATA SHOULD BE RETRIEVED ON BASED ON EMPLOYEE ROLE
+            if (role === "Engineer") {
+                theEmployee = new Engineer (name, id, email, gitHub);
 
-//             });
-
-// }
+            } else if (role === "Intern")
+                theEmployee = new Intern (name, id, email, school);
+                // ADD TO EMPTY TEAM ARRAY TO JOIN ALL INPUT DATA
+                teamData.push(theEmployee);
+            });
+}
