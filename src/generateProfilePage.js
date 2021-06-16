cca132a99568e553f98b9ff3d53d37758de9ae4c
jugrajn <jugraj.nijjar@gmail.com>
2021-06-15 23:23:23 -0400

    render manager html card function created

--- src/generateProfilePage.js
+++ src/generateProfilePage.js
@@ -1,6 +1,19 @@
 // ---------------Create a Function that renders Manager Card---------------
 const renderManager = (manager) => {
-    return ``;
+
+    return `          
+    <div class="col">
+        <div id='teamCard' class="card">
+            <h5>${manager.name}</h5>
+            <h6>Manager</h6>
+        </div>
+
+        <div class='card'>
+            <p>ID: ${manager.id}</p>
+            <p>Email: <a href="${manager.email}">${manager.email}</a></p>
+            <p>Office Number:${manager.officeNumber}</p>
+        </div>
+    </div>`;
 };
 
 
