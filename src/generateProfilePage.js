133f96e325fe74dccf425c1f02a1b96701afe4ce
jugrajn <jugraj.nijjar@gmail.com>
2021-06-16 13:50:45 -0400

    added general html code into generate team function, needs team card template literal

--- src/generateProfilePage.js
+++ src/generateProfilePage.js
@@ -64,5 +64,53 @@ const createTeam = (response) => {
 // ---------------Create a function that renders general HTML Page---------------
 const generateTeamProfile = () => {
     return `
-    `;
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Benne&display=swap" rel="stylesheet">
+    <style>
+
+      h5, h6 {
+        color: #fff;
+        font-family: 'Benne', serif;
+        font-weight: bold;
+        padding-left: 5px;
+      }
+
+      h1 {
+        font-family: 'Benne', serif;
+        font-weight: bold;
+      }
+
+      #teamCard {
+        background-image: linear-gradient(to bottom right, rgb(10, 85, 35), teal);
+      }
+    </style>
+
+</head>
+<body>
+
+      <div class="container">
+
+        <div class="row justify-content-center jumbotron">
+          <div class="col-s12">
+            <h1>Team Profile</h1>
+          </div>
+        </div>
+
+        <div class="row justify-content-space-between">
+        <!--INSERT TEAM CARD ELEMENTS HERE   -->
+        ${}
+        </div>
+
+      </div>
+</body>
+</html>`;
 };
