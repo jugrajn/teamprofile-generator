bfd4cecefb9bad416e952105635a6e0f0075d77a
jugrajn <jugraj.nijjar@gmail.com>
2021-06-15 23:32:39 -0400

    engineer and intern html functions created

--- src/generateProfilePage.js
+++ src/generateProfilePage.js
@@ -10,7 +10,7 @@ const renderManager = (manager) => {
 
         <div class='card'>
             <p>ID: ${manager.id}</p>
-            <p>Email: <a href="${manager.email}">${manager.email}</a></p>
+            <p>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
             <p>Office Number:${manager.officeNumber}</p>
         </div>
     </div>`;
@@ -20,13 +20,39 @@ const renderManager = (manager) => {
 
 // ---------------Create a Function that renders Engineer Card---------------
 const renderEngineer = (engineer) => {
-    return ``;
+
+    return `
+    <div class="col">
+        <div id='teamCard' class='card'>
+            <h5>${engineer.name}</h5>
+            <h6>Engineer</h6>
+        </div>
+
+        <div class='card'>
+            <p>ID: ${engineer.id}</p>
+            <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+            <p>GitHub: <a href="https://github.com/${engineer.gitHub}" target="_blank" rel="noopener noreferrer">${engineer.gitHub}</a></p>
+        </div>
+    </div>`;
 };
 
 
 // ---------------Create a Function that renders Intern Card---------------
 const renderIntern = (intern) => {
-    return ``;
+
+    return `
+    <div class="col">
+        <div id='teamCard' class='card'>
+            <h5>${intern.name}</h5>
+            <h6>Intern</h6>
+        </div>
+
+        <div class='card'>
+            <p>ID: ${intern.id}</p>
+            <p>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+            <p>School:${intern.school}</p>
+        </div>
+    </div>`;
 };
 
 
